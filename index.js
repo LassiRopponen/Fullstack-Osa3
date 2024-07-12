0e3caeee6123215b183fd7ceabd9edca7e597ab6
Lassi Ropponen <lassi.ropponen@tuni.fi>
2024-07-12 12:56:42 +0300

    3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('content', (req, res) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    }
+})
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
     {
