2cc457bcee48bc631393e29b534980f820e7e92b
Lassi Ropponen <lassi.ropponen@tuni.fi>
2024-07-22 13:06:47 +0300

    3.19-3.21

--- index.js
+++ index.js
@@ -78,21 +78,9 @@ const generateId = () => (
     String(Math.floor(Math.random() * 10**6))
 )
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-  
-    if (!body.name) {
-        return response.status(400).json({ 
-            error: 'name missing' 
-        })
-    }
-
-    if (!body.number) {
-        return response.status(400).json({ 
-          error: 'number missing' 
-        })
-    }
-  
+    
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -101,19 +89,21 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const {name, number} = request.body
   
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-  
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, {name, number}, 
+        {new: true, runValidators: true, context: 'query'})
       .then(updatedPerson => {
-        response.json(updatedPerson)
+        if (updatedPerson) {
+            response.json(updatedPerson)
+        }
+        else {
+            response.status(404).end()
+        }
       })
       .catch(error => next(error))
   })
@@ -123,6 +113,9 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } 
+    else if (error.name === "ValidationError") {
+        return response.status(400).json({error: error.message})
     }
   
     next(error)
